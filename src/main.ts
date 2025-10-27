8222b9e65febb542f44894bde1a11247c605626e
Henrique Barucco <henrique_barucco@hotmail.com>
2025-10-27 14:34:07 -0300

    chore: add otel

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-import otelSDK from './tracing';
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { createAppLogger } from './logger/logger';
 
 async function bootstrap() {
-    await otelSDK.start();
-
     const app = await NestFactory.create(AppModule, {
         logger: createAppLogger(),
     });
